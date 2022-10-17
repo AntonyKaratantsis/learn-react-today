2e41b78342757e43c4ce016e38f9ec101d2ee63e
Antonis Karatantsis <karatantsis@gmail.com>
2022-10-17 10:23:54 +0000

    Add state to our Counter rfc

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 const Counter = (props) => {
+  // setCount is an asynchronous function.
+  // Calls to this function are batched instead of run
+  // one after the other
+  const [count, setCount] = useState(Number(props.initialCount));
+
+  // We pass a callback function as an argument is setCount, instead
+  // of passing count + amount as a value, because the operation
+  // depends on the previous state
+  const clickHandler = (amount) => setCount((prevCount) => prevCount + amount);
+
   return (
     <div>
-      <button>-</button>
-      <span> {props.initialCount} </span>
-      <button>+</button>
+      <button onClick={() => clickHandler(-1)}>-</button>
+      <span> {count} </span>
+      <button onClick={() => clickHandler(+1)}>+</button>
     </div>
   );
 };
