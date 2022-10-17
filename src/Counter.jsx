cf57c325d2f6a1e4a3f0c09691d4086d7f487073
Antonis Karatantsis <karatantsis@gmail.com>
2022-10-17 19:04:44 +0300

    Conversion to number not needed in props

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -6,7 +6,7 @@ const Counter = (props) => {
   // setCount is an asynchronous function.
   // Calls to this function are batched instead of run
   // one after the other
-  const [count, setCount] = useState(Number(props.initialCount));
+  const [count, setCount] = useState(props.initialCount);
 
   // We pass a callback function as an argument is setCount, instead
   // of passing count + amount as a value, because the operation
