da23d5f3030f01363ced7b31091cdbba0892cf40
Antonis Karatantsis <karatantsis@gmail.com>
2022-10-17 19:50:24 +0300

    useState with an object

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,24 +3,26 @@ import { useState } from "react";
 const Counter = ({ initialCount }) => {
   console.log("Render Counter");
 
-  // setCount is an asynchronous function.
+  // setState is an asynchronous function.
   // Calls to this function are batched instead of run
   // one after the other
-  const [count, setCount] = useState(initialCount);
+  const [state, setState] = useState({ count: initialCount });
 
-  // We pass a callback function as an argument is setCount, instead
-  // of passing count + amount as a value, because the operation
+  // We pass a callback function as an argument is setState,
+  // because the operation
   // depends on the previous state
-  const clickHandler = (amount) => setCount((prevCount) => prevCount + amount);
-
-  // To demonstrate the error that happens due to the asynchronous nature of useState,
-  // switch the handlers and call clickhandler twice on each button click
-  //    const clickHandler = (amount) => setCount(count + amount);
+  const clickHandler = (amount) =>
+    setState((prevState) => {
+      return {
+        ...prevState,
+        count: prevState.count + amount,
+      };
+    });
 
   return (
     <div>
       <button onClick={() => clickHandler(-1)}>-</button>
-      <span> {count} </span>
+      <span> {state.count} </span>
       <button onClick={() => clickHandler(+1)}>+</button>
     </div>
   );
