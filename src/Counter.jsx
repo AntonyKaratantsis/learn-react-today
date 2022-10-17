eb387f37703760935120280c83de6a3400c83d85
Antonis Karatantsis <karatantsis@gmail.com>
2022-10-17 19:13:49 +0300

    Use object destructuring for simple props

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 
-const Counter = (props) => {
+const Counter = ({ initialCount }) => {
   console.log("Render Counter");
 
   // setCount is an asynchronous function.
   // Calls to this function are batched instead of run
   // one after the other
-  const [count, setCount] = useState(props.initialCount);
+  const [count, setCount] = useState(initialCount);
 
   // We pass a callback function as an argument is setCount, instead
   // of passing count + amount as a value, because the operation
