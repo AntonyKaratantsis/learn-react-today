f78175a8b993f7a5252eb7f5831b0f463fd7a158
Antonis Karatantsis <karatantsis@gmail.com>
2022-10-18 09:54:50 +0000

    Add click handler for changing state of context var

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import Counter from "./Counter";
 
 export const ThemeContext = createContext();
 
 const App = () => {
+  const [theme, setTheme] = useState("red");
+
   return (
-    <ThemeContext.Provider>
+    <ThemeContext.Provider value={{ backgroundColor: theme }}>
       <p>Counter 1</p>
       <Counter initialCount={0} />
       <p>Counter 2</p>
       <Counter initialCount={0} />
+      <br />
+      <button
+        onClick={() =>
+          setTheme((prevTheme) => (prevTheme === "red" ? "blue" : "red"))
+        }
+      >
+        Toggle Theme
+      </button>
     </ThemeContext.Provider>
   );
 };
