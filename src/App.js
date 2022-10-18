fdd5379dc1897712cbe566a6eaaf5316b6c64dbc
Antonis Karatantsis <karatantsis@gmail.com>
2022-10-18 09:41:46 +0000

    Use context API to allow our components to share state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { createContext } from "react";
 import Counter from "./Counter";
 
+export const ThemeContext = createContext();
+
 const App = () => {
-  return <Counter initialCount={0} />;
+  return (
+    <ThemeContext.Provider>
+      <p>Counter 1</p>
+      <Counter initialCount={0} />
+      <p>Counter 2</p>
+      <Counter initialCount={0} />
+    </ThemeContext.Provider>
+  );
 };
 
 export default App;
