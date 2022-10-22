d00f9cb486fb055d7bc5c7d45ff209969ff30d45
Antonis Karatantsis <karatantsis@gmail.com>
2022-10-22 10:40:32 +0000

    Add state to App component for recipe add

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
+import { useState } from "react";
+import { v4 } from "uuid";
 import RecipeList from "./RecipeList";
-import "../css/app.css"
+import "../css/app.css";
 
 const App = () => {
-  console.log("App render");
+  const [recipes, setRecipes] = useState(sampleRecipes);
+
+  const handleRecipeAdd = () => {
+    const newRecipe = {
+      id: v4(),
+      name: "New",
+      servings: 1,
+      cookTime: "1:00",
+      instructions: "Instr.",
+      ingredients: [
+        {
+          id: v4(),
+          name: "Name",
+          amount: "1 Tbsp",
+        },
+      ],
+    };
+
+    setRecipes([...recipes, newRecipe]);
+  };
 
   return (
     <>
-      <RecipeList recipes={sampleRecipes} />
+      <RecipeList recipes={recipes} handleRecipeAdd={handleRecipeAdd}/>
     </>
   );
 };
@@ -17,8 +38,7 @@ const sampleRecipes = [
     name: "Plain Chicken",
     servings: 3,
     cookTime: "1:45",
-    instructions:
-      `1. Put salt on chicken\n2. Put chicken in oven\n3. Eat chicken`,
+    instructions: `1. Put salt on chicken\n2. Put chicken in oven\n3. Eat chicken`,
     ingredients: [
       { id: 1, name: "Chicken", amount: "2 pounds" },
       { id: 2, name: "Salt", amount: "1 tbsp" },
